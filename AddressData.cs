c4acdba3526e7df66753b8615cc3a30c2bd4fe02
VAIBHAV R MANAPURE <106874685+VMANAPURE09@users.noreply.github.com>
2022-07-08 23:51:14 +0530

    Add files via upload
    CSV File JSON Files

---
+++ AddressData.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TPLProject
+{
+    public class AddressData
+    {
+        public string firstName { get; set; }
+
+        public string lastName { get; set; }
+        public string address { get; set; }
+        public string city { get; set; }
+        public string state { get; set; }
+        public string code { get; set; }
+
+
+    }
+}
---
+++ CsvHandler.cs
@@ -0,0 +1,53 @@
+﻿using CsvHelper;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TPLProject
+{
+    public class CsvHandler
+    {
+        public static void ImplementCSVHandling()
+        {
+            string importfilePath = @"E:\BridgeLabz\TPLProject\TPLProject\Book1.csv";
+            // string ExportfilePath = @"E:\BridgeLabz\TPLProject\TPLProject\Export.csv";
+
+            string ExportfilePath = @"E:\BridgeLabz\TPLProject\TPLProject\json1.json";
+
+            using (var reader = new StreamReader(importfilePath))
+            using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
+            {
+                var record = csv.GetRecords<AddressData>().ToList();
+                Console.WriteLine("Read Data successfully from address csv.");
+                foreach (AddressData address in record)
+                {
+                    Console.WriteLine(address.firstName);
+                    Console.WriteLine(address.lastName);
+                    Console.WriteLine(address.address);
+                    Console.WriteLine(address.city);
+                    Console.WriteLine(address.state);
+                    Console.WriteLine(address.code);
+                    Console.WriteLine("-----------------");
+                }
+
+                Console.WriteLine("*************Reading from csv file to csv file************");
+                using (var writer = new StreamWriter(ExportfilePath))
+                using (var csvExport = new CsvWriter(writer, CultureInfo.InvariantCulture))
+                {
+                    csvExport.WriteRecords(record);
+                }
+                //Console.WriteLine("*********Reading from csv file to jSON file*********");
+                //JsonSerializer serializer = new JsonSerializer();
+                //using (StreamWriter sw = new StreamWriter(ExportfilePath))
+                //using(JsonWriter writer = new JsonTextWriter(sw))
+                //{
+                //    serializer.Serialize(writer, record);
+                //}
+            }
+        }
+    }
+}
